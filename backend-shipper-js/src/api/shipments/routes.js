12d8e464c4a72b9f47001e89e4006257d536949a
Aziz <azizmiftakhul19@gmail.com>
2022-03-12 16:41:17 +0700

    update addshipper api

--- backend-shipper-js/src/api/shipments/routes.js
+++ backend-shipper-js/src/api/shipments/routes.js
@@ -1,6 +1,7 @@
 const { allocateShipment } = require('./controller/allocateShipment');
 const { updateShipmentStatus } = require('./controller/updateShipment');
 const { shipment } = require('./controller/shipment');
+const { createShipment } = require('./controller/createShipment');
 
 const router = require('express').Router();
 
@@ -8,5 +9,6 @@ const router = require('express').Router();
 router.post('/allocate/:shipmentNumber', allocateShipment);
 router.put('/allocate/:shipmentNumber', updateShipmentStatus);
 router.get('/all', shipment);
+router.post('/addShipment', createShipment);
 
 module.exports = router;
